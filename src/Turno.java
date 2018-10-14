12e5a0f60eafc69cdad6bb0f11e988b017351576
luisgm21 <42901084+luisgm21@users.noreply.github.com>
2018-10-14 19:59:51 -0300

    Update src/Turno.java

--- src/Turno.java
+++ src/Turno.java
@@ -1,35 +1,4 @@
 
-import java.time.LocalDateTime;
 
 
-public class Turno {
-    private int numturn;
-    private String nombremedico;
-    private LocalDateTime fechayhora;
-    public Turno(int numturn,String nombremedico,LocalDateTime fechayhora){
-        this.numturn=numturn;
-        this.nombremedico=nombremedico;
-        this.fechayhora=fechayhora;
-    }
-    public Turno(){
-        this.fechayhora=null;
-    }
-    public int getNumturn() {
-        return numturn;
-    }
-    public void setNumturn(int numturn) {
-        this.numturn = numturn;
-    }
-    public String getNombremedico() {
-        return nombremedico;
-    }
-    public void setNombremedico(String nombremedico) {
-        this.nombremedico = nombremedico;
-    }
-    public LocalDateTime getFechayhora() {
-        return fechayhora;
-    }
-    public void setFechayhora(LocalDateTime fechayhora) {
-        this.fechayhora = fechayhora;
-    }
 }
